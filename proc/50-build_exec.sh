c698f2cbeff58a2781cad128fb32581b571d63fa
Mingcong Bai <jeffbai@aosc.xyz>
2020-05-28 16:22:52 -0500

    build/50-build_exec: move QA out

--- proc/50-build_exec.sh
+++ proc/50-build_exec.sh
@@ -27,11 +27,7 @@ done
 build_${ABTYPE}_build || abdie "Build failed: $?."
 
 [ -d "$PKGDIR" ] || abdie "50-build: Suspecting build failure due to missing PKGDIR."
-#[ -d "`arch_findfile overrides`" ] && cp -rla "`arch_findfile overrides`"/* "$PKGDIR/"
 [ -d "`arch_findfile overrides`" ] && cp -ra "`arch_findfile overrides`"/* "$PKGDIR/"
 arch_loadfile_strict beyond
 
 unalias BUILD_{START,READY,FINAL}
-
-# Inline: QA Dir
-bool $ABQA && [ -e $PKGDIR/usr/local ] && abicu "QA: found \$PKGDIR/usr/local."
