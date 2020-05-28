09dd4186a635e066376f61e27411d5eae4f7562b
Mingcong Bai <jeffbai@aosc.xyz>
2020-05-28 16:34:08 -0500

    qa/30-path_issues: implement more bad-path detection

--- qa/30-path_issues.sh
+++ qa/30-path_issues.sh
@@ -2,5 +2,8 @@
 ##30-path_issues: Check for incorrectly installed paths.
 ##@copyright GPL-2.0+
 
-[ -e "$PKGDIR"/usr/local ] && abdie "QA: found \$PKGDIR/usr/local."
-
+for i in bin sbin lib lib64 run usr/sbin usr/lib64 \
+         usr/local usr/man usr/doc usr/etc usr/var; do
+	[ -e "$PKGDIR"/${i} ] && \
+		abdie "QA: Found \$PKGDIR/$i."
+done
